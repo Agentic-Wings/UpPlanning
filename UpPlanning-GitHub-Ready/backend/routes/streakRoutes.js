fc3e66508d361dbb5119da942dd286a96c75e8e6
Agentic-Wings <agent48ai@gmail.com>
2026-07-02 20:21:28 +0700

    Delete UpPlanning-GitHub-Ready directory

--- UpPlanning-GitHub-Ready/backend/routes/streakRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const streakController = require('../controllers/streakController');
-
-router.get('/', streakController.getStreak);
-router.post('/record', streakController.recordActivity);
-
-module.exports = router;
